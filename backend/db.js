c9efe48c4e79744bf8a88a5319a441849e803790
unknown <tpcenter13@gmail.com>
2025-02-19 22:41:38 +0800

    fix

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 require('dotenv').config
  
 const db = mysql.createConnection({
-  host: 'bdnfyppmyhuekgygkgo5-mysql.services.clever-cloud.com', // XAMPP default
-  user: 'uitesgrbdgredt2g',      // Default user
-  password: 'y5TFAFvt5rcCsjowUjO1',      // No password (unless set)
-  database: 'bdnfyppmyhuekgygkgo5', // Replace with your actual DB name
+  host: process.env.HOST , // XAMPP default
+  user: process.env.USER,      // Default user
+  password: process.env.PASSWORD,      // No password (unless set)
+  database: process.env.DATABASE, // Replace with your actual DB name
 });
  
 // Connect to MySQL
